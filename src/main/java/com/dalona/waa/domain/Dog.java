2a60cc9cf291b900627b2417864fe638849df7fa
minseo <alstj778@gmail.com>
2025-02-27 21:21:24 +0900

    fix: update age to birthdate

--- src/main/java/com/dalona/waa/domain/Dog.java
+++ src/main/java/com/dalona/waa/domain/Dog.java
@@ -3,21 +3,23 @@ package com.dalona.waa.domain;
 import com.dalona.waa.enums.DogGender;
 import com.dalona.waa.enums.DogStatus;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name="dog")
+@Table(name = "dog")
 public class Dog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="organization_id", nullable = false)
+    @Column(name = "organization_id", nullable = false)
     private Integer organizationId;
 
     @Column(nullable = false)
@@ -27,23 +29,26 @@ public class Dog {
     @Column(name = "dog_gender", nullable = false)
     private DogGender gender;
 
-    @Column(nullable = false)
-    private Integer age;
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "birth_date_is_estimated", nullable = false)
+    private Boolean birthDateIsEstimated;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "dog_status", nullable = false)
     private DogStatus status;
 
-    @Column(name="created_at", nullable = false)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name="updated_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @Column(name="created_by", nullable = false)
+    @Column(name = "created_by", nullable = false)
     private Integer createdBy;
 
-    @Column(name="updated_by")
+    @Column(name = "updated_by")
     private Integer updatedBy;
 
     @PrePersist
@@ -58,5 +63,13 @@ public class Dog {
     }
 
     @Builder
-    public Dog() {}
+    public Dog(int organizationId, String name, DogGender gender, LocalDate birthDate, boolean birthDateIsEstimated,
+               DogStatus status) {
+        this.organizationId = organizationId;
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.birthDateIsEstimated = birthDateIsEstimated;
+        this.status = status;
+    }
 }
