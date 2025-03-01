9e5adb760d534e21c36a6e0570befbed795a5d88
minseo <alstj778@gmail.com>
2025-03-01 23:26:54 +0900

    fix: remove default array value

--- src/main/java/com/dalona/waa/dto/clovaDto/ClovaRequestBody.java
+++ src/main/java/com/dalona/waa/dto/clovaDto/ClovaRequestBody.java
@@ -8,12 +8,12 @@ import lombok.Data;
 @Builder
 public class ClovaRequestBody {
 
-    private ArrayList<ClovaChatMessage> messages = new ArrayList<>();
+    private ArrayList<ClovaChatMessage> messages;
     private Double temperature;
     private Integer topK;
     private Double topP;
     private Double repeatPenalty;
-    private ArrayList<String> stopBefore = new ArrayList<>();
+    private ArrayList<String> stopBefore;
     private Integer maxTokens;
     private Boolean includeAiFilters;
     private Integer seed;
