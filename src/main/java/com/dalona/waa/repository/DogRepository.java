5de4c68b790c436279e10eb8783c05a18f68e3ab
minseo <alstj778@gmail.com>
2025-03-02 00:16:53 +0900

    feat: implement dog list api

--- src/main/java/com/dalona/waa/repository/DogRepository.java
+++ src/main/java/com/dalona/waa/repository/DogRepository.java
@@ -1,9 +1,12 @@
 package com.dalona.waa.repository;
 
 import com.dalona.waa.domain.Dog;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DogRepository extends JpaRepository<Dog, Integer> {
+
+    List<Dog> findAllByOrganizationId(Integer organizationId);
 }
--- src/main/java/com/dalona/waa/service/DogService.java
+++ src/main/java/com/dalona/waa/service/DogService.java
@@ -2,12 +2,16 @@ package com.dalona.waa.service;
 
 import com.dalona.waa.domain.Dog;
 import com.dalona.waa.domain.DogProfile;
+import com.dalona.waa.dto.responseDto.DogInfoResDto;
 import com.dalona.waa.dto.responseDto.DogProfileResDto;
 import com.dalona.waa.dto.responseDto.DogResDto;
 import com.dalona.waa.repository.DogProfileRepository;
 import com.dalona.waa.repository.DogRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -31,5 +35,20 @@ public class DogService {
         DogProfile dogProfile = dogProfileRepository.findById(dogId)
                 .orElseThrow(() -> new EntityNotFoundException("DOG_PROFILE_NOT_FOUND"));
         return new DogProfileResDto(dogProfile);
+    }
+
+    public List<DogInfoResDto> getDogInfoListByOrganizationId(Integer organizationId) {
+        List<Dog> dogs = dogRepository.findAllByOrganizationId(organizationId);
+        List<Integer> dogIds = dogs.stream()
+                .map(Dog::getId)
+                .toList();
+        List<DogProfile> dogProfiles = dogProfileRepository.findByDogIdIn(dogIds);
+
+        Map<Integer, DogProfile> profileMap = dogProfiles.stream()
+                .collect(Collectors.toMap(DogProfile::getDogId, profile -> profile));
+
+        return dogs.stream()
+                .map(dog -> new DogInfoResDto(dog, profileMap.get(dog.getId())))
+                .collect(Collectors.toList());
     }
 }
