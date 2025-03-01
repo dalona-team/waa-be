8d4f83789189d24ad0e4692d53c86aeba4d863b5
minseo <alstj778@gmail.com>
2025-03-01 23:26:27 +0900

    feat: add default api

---
+++ src/main/java/com/dalona/waa/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.dalona.waa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to WAA API!";
+    }
+}
